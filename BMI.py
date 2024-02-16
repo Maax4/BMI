86cc89cd029b4f00ca1c1f92fc4228dc5253ee31
Maax4 <160223115+Maax4@users.noreply.github.com>
2024-02-16 18:03:01 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -16,30 +16,20 @@ weight = st.number_input("Enter your weight in KG", step = 0.1)
 
 height = st.number_input("Enter your height in Meters", step = 0.01)
 
-class BMI:
-    def __init__(self, height_value, weight_value):
+def calculate_BMI_and_display_weight_status(self):
 
-      self.height_value = height_value
-      self.weight_value = weight_value
+  BMI_value = self.weight_value / self.height_value ** 2
+  return BMI_value
 
-    def calculate_BMI(self):
-
-      BMI_value = self.weight_value / self.height_value ** 2
-      return BMI_value
-
-    def displayweightstatus(self):
-
-      BMI_value = self.weight_value / self.height_value ** 2
-
-      if BMI_value < 18.5:
-        print('You are underweight, skinny.')
-      elif BMI_value >= 18.5 and BMI_value <= 22.9:
-        print('You have normal weight.')
-      elif BMI_value > 22.9 and BMI_value <= 29.9:
-        print('You are overweight, fatso.')
-      else:
-        print('You are obese, fatty bom bom.')
+  if BMI_value < 18.5:
+    print('You are underweight, skinny.')
+  elif BMI_value >= 18.5 and BMI_value <= 22.9:
+    print('You have normal weight.')
+  elif BMI_value > 22.9 and BMI_value <= 29.9:
+    print('You are overweight, fatso.')
+  else:
+    print('You are obese, fatty bom bom.')
 
 button = st.button("Calculate BMI")
 if button:
-    calculate_BMI()
+    calculate_BMI_and_display_weight_status()
